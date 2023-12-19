9b3f95e7959bcda22c71e3aefee16eb5a7ff19e0
David Jeronimo <david.jeronimo@version1.com>
2023-12-19 15:31:30 +0100

    aoc-19: aplenty

--- README.md
+++ README.md
@@ -18,4 +18,6 @@
 | 14  | [Parabolic reflector dish](https://adventofcode.com/2023/day/14)       | 0.007          | 0.934          | [Aoc14.hs](src/Aoc14.hs) |
 | 15  | [Lens library](https://adventofcode.com/2023/day/15)                   | 0.004          | 0.015          | [Aoc15.hs](src/Aoc15.hs) |
 | 16  | [The floor will be lava](https://adventofcode.com/2023/day/16)         | 0.027          | 4.244          | [Aoc16.hs](src/Aoc16.hs) |
-| 16  | [Clumsy crucible](https://adventofcode.com/2023/day/17)                | 8.121          | 35.705         | [Aoc17.hs](src/Aoc17.hs) |
+| 17  | [Clumsy crucible](https://adventofcode.com/2023/day/17)                | 8.121          | 35.705         | [Aoc17.hs](src/Aoc17.hs) |
+| 18  |                                                                        |                |                |                          |
+| 19  | [Aplenty](https://adventofcode.com/2023/day/19)                        | 0.016          | 0.016          | [Aoc19.hs](src/Aoc19.hs) |
---
+++ src/Aoc19.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Aoc19 (parseInput, solution) where
+
+import Lib (Part(..), Text)
+import qualified Data.Text as T
+import Data.List.Split (chunksOf)
+import Data.Map (Map, (!))
+import qualified Data.Map as M
+import Data.Char (toUpper)
+import Data.Maybe (fromMaybe)
+import Data.Bifunctor (bimap)
+import Data.Monoid (First(..))
+
+data Prop     = X | M | A | S deriving (Show, Read, Eq, Ord)
+data Result   = Accepted | Rejected deriving (Show, Eq)
+data Target   = TR Result | TW Text deriving (Show, Eq)
+data Rule     = Rule { prop::Prop, cmp::Ordering, val::Int, target::Target } deriving Show
+data Workflow = Workflow { rules:: [Rule], fallback::Target} deriving Show
+
+type PR     = Map Prop Int
+type Rng    = Map Prop (Int,Int)
+type MinMax = (Int,Int)
+
+parseInput::Text -> (Map Text Workflow, [PR])
+parseInput = bimap (M.fromList . parseWorkflows) (map parsePart . drop 2 . T.lines) . T.breakOn "\n\n"
+  where parseWorkflows txt = [(wn,parseWorkflow rls) | wn:rls <- map (init . T.split (`T.elem` ":,{}")) . T.lines $ txt ]
+        parseWorkflow rls = Workflow (map (parseRule . map T.unpack) . chunksOf 2 . init $ rls) (parseTarget . T.unpack . last $ rls)
+        parseRule [p:cmp:v,tgt] = Rule (read [toUpper p]) (parseCmp cmp) (read v) (parseTarget tgt)
+        parseRule _               = error "Invalid rule"
+        parseCmp '>' = GT
+        parseCmp _   = LT
+        parseTarget "A" = TR Accepted
+        parseTarget "R" = TR Rejected
+        parseTarget tgt = TW . T.pack $ tgt
+        parsePart = M.fromList . map (bimap (read . T.unpack . T.toTitle) (read . T.unpack . T.tail) . T.breakOn "=") . T.splitOn "," . T.init . T.tail
+
+solution::Part -> (Map Text Workflow, [PR]) -> Int
+solution part (workflows, parts) = case part of 
+      PartOne -> sum . map points . filter ((== Accepted) . processPart workflows "in") $ parts
+      PartTwo -> sum . map sumCombinations $ combinations workflows ("in",initRange)
+  where points = sum . M.elems
+        initRange = M.fromList [(X,(1,4000)),(M,(1,4000)),(A,(1,4000)),(S,(1,4000))]  
+        sumCombinations = product . map (\(a,b) -> b - a + 1) . M.elems 
+
+processPart::Map Text Workflow -> Text -> PR -> Result
+processPart workflows wfTitle pr = case applyWorkflow (workflows!wfTitle) pr of
+      TR result -> result
+      TW nextWf -> processPart workflows nextWf pr
+  where applyWorkflow Workflow{..} parts = fromMaybe fallback . getFirst . foldMap (First . applyRule parts) $ rules
+
+applyRule::PR -> Rule -> Maybe Target
+applyRule part Rule{..} | success   = Just target
+                        | otherwise = Nothing
+  where success = compare (part ! prop) val == cmp
+
+combinations::Map Text Workflow -> (Text,Rng) -> [Rng]
+combinations workflows (wfName, range) = concatMap progress $ validRanges workflows (wfName, range)
+  where progress (_,   TR Rejected) = []
+        progress (rgn, TR Accepted) = [rgn]
+        progress (rgn, TW wfName')  = combinations workflows (wfName',rgn)
+
+validRanges::Map Text Workflow -> (Text,Rng) -> [(Rng, Target)]
+validRanges workflows (wfName, range) = applyRules rules range
+  where Workflow{..} = workflows!wfName
+        applyRules (rule:rules') range' = positiveRule range' rule : applyRules rules' (negativeRule range' rule)
+        applyRules []            range' = [(range', fallback)]
+        positiveRule range' rule@Rule{..} = (applyConstraint range' prop (rangesForRule True rule), target)   
+        negativeRule range' rule@Rule{..} = applyConstraint range' prop . rangesForRule False $ rule
+
+applyConstraint::Rng -> Prop -> MinMax -> Rng
+applyConstraint rng p (a,b) = updateRangeEntry $ rng ! p 
+  where updateRangeEntry (a1,b1) = M.insert p (max a1 a, min b1 b) rng
+
+rangesForRule::Bool -> Rule -> MinMax
+rangesForRule True  Rule{cmp=LT,..} = (minBound, val - 1)
+rangesForRule True  Rule{cmp=GT,..} = (val + 1, maxBound)
+rangesForRule False Rule{cmp=LT,..} = (val, maxBound)
+rangesForRule _     Rule{..}        = (minBound, val)
